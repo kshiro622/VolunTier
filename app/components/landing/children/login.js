7def82a0333b608915507edc0325501ec7ef7c34
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-03-17 17:15:21 -0500

    adds login component

--- app/components/landing/children/login.js
+++ app/components/landing/children/login.js
@@ -3,13 +3,35 @@ var React = require("react");
 
 // Creating the Form component
 var Login = React.createClass({
-
     render: function () {
         return (
             <span>
-                test
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-4 col-md-offset-4">
+                            <div className="login-panel panel panel-default">
+                                <div className="panel-heading">
+                                    <h3 className="panel-title">Please Sign In</h3>
+                                </div>
+                                <div className="panel-body">
+                                    <form role="form">
+                                        <fieldset>
+                                            <div className="form-group">
+                                                <input className="form-control" placeholder="E-mail" name="email" type="email" autoFocus />
+                                            </div>
+                                            <div className="form-group">
+                                                <input className="form-control" placeholder="Password" name="password" type="password" value="" />
+                                            </div>
+                                            <button href="index.html" className="btn btn-lg btn-success btn-block">Login</button>
+                                        </fieldset>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </span>
-        )
+        );
     }
 });
 

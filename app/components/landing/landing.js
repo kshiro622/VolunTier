83c3b7a68663b9085722289434ed6f37840c32fa
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-03-19 14:23:46 -0500

    finishes styling of the login dropdown on the landing page

--- app/components/landing/landing.js
+++ app/components/landing/landing.js
@@ -28,7 +28,36 @@ var Landing = React.createClass({
     render: function () {
         return (
             <span>
-                <Login />
+                <nav className="navbar navbar-default navbar-static-top" role="navigation" style={{ marginBottom: 0 }}>
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <a className="navbar-brand" href="index.html">Do Good</a>
+                    </div>
+                    <Login />
+                </nav>
+
+                <footer className="navbar navbar-default navbar-fixed-bottom" role="navigation" style={{ marginBottom: 0 }}>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-sm-12 footer-txt">
+                                Made by&nbsp;&nbsp;
+                                <a target="_blank" href="https://github.com/JustinRyanCarlson">Justin Carlson</a>
+                                <span>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;</span>
+                                <a target="_blank" href="https://github.com/aishaprograms">Aisha Ahmad</a>
+                                <span>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;</span>
+                                <a target="_blank" href="https://github.com/kshiro622">Katie Shiro</a>
+                                <span>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;</span>
+                                <a target="_blank" href="https://github.com/hadicodes">Hadi</a>
+                                <span className="pull-right">The Do Good App &copy; 2017</span>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
             </span>
         );
     }

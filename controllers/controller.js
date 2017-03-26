e262c187b0c294376169e0364890542c66546380
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-03-26 17:22:30 -0500

    adds my route

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var passport = require('passport');
 var user = require('./../models/user.js');
 var Goal = require('./../models/goal.js');
+var Event = require('./../models/event.js');
 
 
 // =======================================================
@@ -15,9 +16,7 @@ router.post('/register', function (req, res) {
         first_name: req.body.first_name,
         last_name: req.body.last_name,
         bio: req.body.bio,
-        goal_week_goal: req.body.goal_week,
-        goal_month_goal: req.body.goal_week * 4,
-        goal_year_goal: req.body.goal_week * 52,
+        goal_week: req.body.goal_week,
         interests: req.body.interests
     }), req.body.password, function (err, user) {
         if (err) {
@@ -159,6 +158,96 @@ router.get("/user/goaltracker/:id", function (req, res) {
 
         res.send(user);
     })
+});
+
+// =======================================================
+// Upcoming Events routes
+// =======================================================
+// get all events by user id
+router.get("/api/events/:userId", function (req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the
+    // matching one in our db...
+    user.findOne({ "_id": req.params.userId })
+        // ..and populate all of the events associated with it
+        .populate("events")
+        // now, execute our query
+        .exec(function (error, doc) {
+            // Log any errors
+            if (error) {
+                console.log(error);
+            } else {
+                // Otherwise, send the doc to the browser as a json object
+                res.json(doc);
+            }
+        });
+});
+
+// Create a new Event
+router.post("/api/events/:userId", function (req, res) {
+    // Create a new Event and pass the req.body to the entry
+    var createEvent = Event.create(req.body, function (error, eventDoc) {
+        // Log any errors
+        if (error) {
+            console.log(error);
+        }
+    });
+    createEvent.then(function (response) {
+        // Use the User id to find and update it's Event
+        user.findOneAndUpdate({
+            "_id": req.params.userId
+        }, {
+                $push: {
+                    "events": response._id
+                }
+            }, {
+                safe: true,
+                upsert: true,
+                new: true
+            })
+            // ..and populate all of the events associated with it
+            .populate("events")
+            // Execute the above query
+            .exec(function (err, doc) {
+                // Log any errors
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.json(doc);
+                }
+            });
+    });
+});
+
+router.delete("/api/events/:userId/:eventId", function (req, res) {
+    Event.findByIdAndRemove(req.params.eventId, function (error, eventDoc) {
+        // Log any errors
+        if (error) {
+            console.log(error);
+        } else {
+            user.findOneAndUpdate({
+                "_id": req.params.userId
+            }, {
+                    $pull: {
+                        "events": eventDoc._id
+                    }
+                }, {
+                    safe: true,
+                    new: true
+                })
+                // ..and populate all of the events associated with it
+                .populate("events")
+                // Execute the above query
+                .exec(function (err, doc) {
+                    // Log any errors
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        res.json(doc);
+                    }
+                });
+        }
+    });
 });
 
 // Export routes for server.js to use.

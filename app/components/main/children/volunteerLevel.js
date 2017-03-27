3da757c68c4259cbdd13adba509c73a27b1e2cb3
Katherine Shiro <katie.shiro@gmail.com>
2017-03-27 16:26:51 -0500

    updates volunteerLevel so that conditional statement is easier to read

--- app/components/main/children/volunteerLevel.js
+++ app/components/main/children/volunteerLevel.js
@@ -30,17 +30,58 @@ var VolunteerLevel = React.createClass({
 
         var currentLevel = null;
         if (tier === 0) {
-            currentLevel = (<div><h2>Beginner Level</h2><img src="assets/images/tier0.png" height="130px" alt="Level 0 badge" /><p>Start getting some hours in!</p></div>);
+            currentLevel = (
+                <div>
+                    <h2>Beginner Level</h2>
+                    <img src="assets/images/tier0.png" height="130px" alt="Level 0 badge" />
+                    <p>Start getting some hours in!</p>
+                </div>
+            );
         } else if (1 <= tier && tier < 10) {
-            currentLevel = (<div><h2>Level 1</h2><p>1-9 hours</p><img src="assets/images/tier1.png" height="130px" alt="Level 1 badge" /> <h5 className="level-text">Great start! You're already making a difference. Every hour counts!</h5></div>);
+            currentLevel = (
+                <div>
+                    <h2>Level 1</h2>
+                    <p>1-9 hours</p>
+                    <img src="assets/images/tier1.png" height="130px" alt="Level 1 badge" />
+                    <h5 className="level-text">Great start! You're already making a difference. Every hour counts!</h5>
+                </div>
+            );
         } else if (10 <= tier && tier < 20) {
-            currentLevel = (<div><h2>Level 2</h2><p>10-19 hours</p> <img src="assets/images/tier2.png" height="130px" alt="Level 2 badge" /><h5 className="level-text">You're really making an impact!</h5></div>);
+            currentLevel = (
+                <div>
+                    <h2>Level 2</h2>
+                    <p>10-19 hours</p>
+                    <img src="assets/images/tier2.png" height="130px" alt="Level 2 badge" />
+                    <h5 className="level-text">You're really making an impact!</h5>
+                </div>
+            );
         } else if (20 <= tier && tier < 50) {
-            currentLevel = (<div><h2>Level 3</h2><p>20-49 hours</p> <img src="assets/images/tier3.png" height="130px" alt="Level 3 badge" /><h5 className="level-text">Now you can really start to see what you've accomplished!</h5></div>);
+            currentLevel = (
+                <div>
+                    <h2>Level 3</h2>
+                    <p>20-49 hours</p>
+                    <img src="assets/images/tier3.png" height="130px" alt="Level 3 badge" />
+                    <h5 className="level-text">Now you can really start to see what you've accomplished!</h5>
+                </div>
+            );
         } else if (50 <= tier && tier < 100) {
-            currentLevel = (<div><h2>Level 4</h2><p>50-99 hours</p> <img src="assets/images/tier4.png" height="130px" alt="Level 4 badge" /><h5 className="level-text">What an amazing feat, you're almost to the top!</h5></div>);
+            currentLevel = (
+                <div>
+                    <h2>Level 4</h2>
+                    <p>50-99 hours</p>
+                    <img src="assets/images/tier4.png" height="130px" alt="Level 4 badge" />
+                    <h5 className="level-text">What an amazing feat, you're almost to the top!</h5>
+                </div>
+            );
         } else if (100 <= tier) {
-            currentLevel = (<div><h2>Level 5</h2><p>100 hours or more</p> <img src="assets/images/tier5.png" height="130px" alt="Level 5 badge" /><h5 className="level-text">You've reached the top! The world is a better place because of you!</h5></div>);
+            currentLevel = (
+                <div>
+                    <h2>Level 5</h2>
+                    <p>100 hours or more</p>
+                    <img src="assets/images/tier5.png" height="130px" alt="Level 5 badge" />
+                    <h5 className="level-text">You've reached the top! The world is a better place because of you!</h5>
+                </div>
+            );
         }
 
         return (

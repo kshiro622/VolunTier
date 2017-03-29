2669c53c9aa48316ba45a65c97b04a692d0c14b9
Katherine Shiro <katie.shiro@gmail.com>
2017-03-28 22:16:16 -0500

    adds axios call to set states for profile component

--- app/components/profile/profile.js
+++ app/components/profile/profile.js
@@ -1,6 +1,7 @@
 // Include the Main React Dependency
 var React = require("react");
 var axios = require("axios");
+var eventHelper = require("../../utils/eventsHelper.js");
 import { Link } from 'react-router'
 
 // Include children components
@@ -12,25 +13,116 @@ var Profile = React.createClass({
     getInitialState: function () {
         return {
             username: "",
-            first_name: "",
-            last_name: "",
+            name: "",
             image_url: "",
             bio: "",
+            past_events: "",
+            upcoming_events: "",
             interest1: "",
             interest2: "",
             interest3: "",
             goal_week: "",
             goal_week_current: "",
             goal_year_current: "",
+            level: "",
+            events: ""
         };
     },
 
     componentWillMount: function () {
-        //axios call to get user information
+        var currentUser = sessionStorage.getItem('do_good_id');
+        var userRoute = '/user/goaltracker/' + currentUser;
+
+        axios.get(userRoute)
+            .then(function (response) {
+                var name = response.data.first_name + " " + response.data.last_name;
+                var totalHoursThisYear = response.data.goal_year_current;
+
+                this.setState({
+                    name: name,
+                    username: response.data.username,
+                    image_url: response.data.image_url,
+                    bio: response.data.bio,
+                    past_events: pastEvents,
+                    upcoming_events: upcomingEvents,
+                    interest1: response.data.interests[0],
+                    interest2: response.data.interests[1],
+                    interest3: response.data.interests[2],
+                    goal_week: response.data.goal_week_goal,
+                    goal_week_current: response.data.goal_week_current,
+                    goal_year_current: response.data.goal_year_current,
+                    level: totalHoursThisYear
+                });
+
+            }.bind(this));
+
+        eventHelper.getSavedEvents(currentUser).then(function (response) {
+            var pastEvents;
+            var upcomingEvents;
+            this.setState({ events: response.data.events });
+        }.bind(this));
     },
 
     // Here we render the function
     render: function () {
+        var tier = this.state.level;
+
+        var currentLevel = null;
+        if (tier === 0) {
+            currentLevel = (
+                <div>
+                    <h2>Beginner Level</h2>
+                    <img src="assets/images/tier0.png" height="150px" className="margin-bottom-30 margin-top-20" alt="Level 0 badge" />
+                    <p>Start getting some hours in!</p>
+                </div>
+            );
+        } else if (1 <= tier && tier < 10) {
+            currentLevel = (
+                <div>
+                    <h2>Level 1</h2>
+                    <p>1-9 hours</p>
+                    <img src="assets/images/tier1.png" height="150px" className="margin-bottom-30 margin-top-20" alt="Level 1 badge" />
+                    <h5 className="level-text">Great start! You are already making a difference. Every hour counts!</h5>
+                </div>
+            );
+        } else if (10 <= tier && tier < 20) {
+            currentLevel = (
+                <div>
+                    <h2>Level 2</h2>
+                    <p>10-19 hours</p>
+                    <img src="assets/images/tier2.png" height="150px" className="margin-bottom-30 margin-top-20" alt="Level 2 badge" />
+                    <h5 className="level-text">You are really making an impact!</h5>
+                </div>
+            );
+        } else if (20 <= tier && tier < 50) {
+            currentLevel = (
+                <div>
+                    <h2>Level 3</h2>
+                    <p>20-49 hours</p>
+                    <img src="assets/images/tier3.png" height="150px" className="margin-bottom-30 margin-top-20" alt="Level 3 badge" />
+                    <h5 className="level-text">Now you can really start to see what you have accomplished!</h5>
+                </div>
+            );
+        } else if (50 <= tier && tier < 100) {
+            currentLevel = (
+                <div>
+                    <h2>Level 4</h2>
+                    <p>50-99 hours</p>
+                    <img src="assets/images/tier4.png" height="150px" className="margin-bottom-30 margin-top-20" alt="Level 4 badge" />
+                    <h5 className="level-text">What an amazing feat, you are almost to the top!</h5>
+                </div>
+            );
+        } else if (100 <= tier) {
+            currentLevel = (
+                <div>
+                    <h2>Level 5</h2>
+                    <p>100 hours or more</p>
+                    <img src="assets/images/tier5.png" height="150px" className="margin-bottom-30 margin-top-20" alt="Level 5 badge" />
+                    <h5 className="level-text">You have reached the top! The world is a better place because of you!</h5>
+                </div>
+            );
+        }
+
         return (
             <span>
                 <nav className="main-nav navbar navbar-default navbar-static-top" role="navigation" >
@@ -47,11 +139,23 @@ var Profile = React.createClass({
                         </div>
                         <ul className="nav navbar-top-links navbar-right">
                             <li className="dropdown">
-                                <Link to="/" className="scroll-link">
-                                    Home
-                                </Link>
+                                <a className="dropdown-toggle" data-toggle="dropdown" href="#">
+                                    <i className="fa fa-user fa-fw white-icon"></i> <i className="fa fa-caret-down white-icon"></i>
+                                </a>
+                                <ul className="dropdown-menu dropdown-user">
+                                    <li>
+                                        <Link to="/" className="scroll-link">
+                                            <i className="fa fa-dashboard fa-fw"></i>
+                                            Dashboard
+                                         </Link>
+                                    </li>
+                                    <li><a href="#"><i className="fa fa-gear fa-fw"></i> Settings</a>
+                                    </li>
+                                    <li className="divider"></li>
+                                    <li><a onClick={this.logout}><i className="fa fa-sign-out fa-fw"></i> Logout</a>
+                                    </li>
+                                </ul>
                             </li>
-                            <Login />
                         </ul>
                     </div>
                 </nav>
@@ -63,21 +167,21 @@ var Profile = React.createClass({
                                 <img src="assets/images/defaultuser.png" className="img-circle margin-top-50" height="200px" alt="" />
                             </div>
                             <div className="col-md-9">
-                                <h1 className="margin-top-80">Name</h1>
+                                <h1 className="margin-top-80">{this.state.name}</h1>
                                 <div className="row">
-                                    <div className="col-md-3">
-                                        <h4 className="text-orange">username</h4>
-                                        <h4>Weekly Goal:</h4>
+                                    <div className="col-md-4">
+                                        <h4 className="text-orange">{this.state.username}</h4>
                                     </div>
-                                    <div className="col-md-6">
-                                        <h4>Goal Progress:</h4>
-                                        <h4>Total hours this year:</h4>
+                                    <div className="col-md-5">
+                                        <h4>Weekly Goal: {this.state.goal_week} hrs</h4>
+                                        <h4>Goal Progress: {this.state.goal_week_current} hrs</h4>
+                                        <h4>Total Hours This Year: {this.state.goal_year_current} hrs</h4>
                                     </div>
                                     <div className="col-md-3">
                                         <h4>Interests:</h4>
-                                        <h6>Interest1</h6>
-                                        <h6>Interest2</h6>
-                                        <h6>Interest3</h6>
+                                        <h6>{this.state.interest1}</h6>
+                                        <h6>{this.state.interest2}</h6>
+                                        <h6>{this.state.interest3}</h6>
                                     </div>
                                 </div>
                             </div>
@@ -93,7 +197,7 @@ var Profile = React.createClass({
                                         About:
                                         </div>
                                     <div className="panel-body">
-                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                        <p>{this.state.bio}</p>
                                     </div>
                                 </div>
                             </div>
@@ -102,8 +206,8 @@ var Profile = React.createClass({
                                     <div className="panel-heading">
                                         Level
                                     </div>
-                                    <div className="panel-body">
-                                        <p>Level info here</p>
+                                    <div className="panel-body center-align">
+                                        {currentLevel}
                                     </div>
                                 </div>
                             </div>
